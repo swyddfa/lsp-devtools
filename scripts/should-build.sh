6aa9572c4fab38798e13e8245eb301faaac1b833
Alex Carney <alcarneyme@gmail.com>
2022-03-28 18:47:11 +0100

    Just enough automation

---
+++ scripts/should-build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Script to check if we should build a given component or not.
+
+# File patterns to check for each component, if there's a match a build will be
+# triggered
+PYTEST_LSP="^lib/pytest-lsp/"
+
+# Determine which files have changed
+git diff --name-only ${BASE}..HEAD -- >> changes
+echo -e "Files Changed:\n"
+cat changes
+
+case $1 in
+    pytest-lsp)
+        PATTERN=${PYTEST_LSP}
+        ;;
+    *)
+        echo "Unknown component ${1}"
+        exit 1
+        ;;
+esac
+
+changes=$(grep -E "${PATTERN}" changes)
+echo
+
+rm changes
+
+if [ -z "$changes" ]; then
+    echo "There is nothing to do."
+else
+    echo "Changes detected, doing build!"
+    echo "::set-output name=build::true"
+fi
