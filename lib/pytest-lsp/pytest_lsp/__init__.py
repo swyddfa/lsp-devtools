eb0c1e2001c0851a26ff2fb96bc8af9a89d6b1c3
Alex Carney <alcarneyme@gmail.com>
2022-03-28 23:13:19 +0100

    pytest-lsp: Bump version

--- lib/pytest-lsp/pytest_lsp/__init__.py
+++ lib/pytest-lsp/pytest_lsp/__init__.py
@@ -5,7 +5,7 @@ from .plugin import make_client_server
 from .plugin import ClientServer
 from .plugin import ClientServerConfig
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 __all__ = [
     "Client",
