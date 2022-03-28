f77d2fe599ac0d128f486a442b916b82ff878340
Alex Carney <alcarneyme@gmail.com>
2022-03-28 23:12:10 +0100

    pytest-lsp: Remove event_loop fixture

--- lib/pytest-lsp/pytest_lsp/__init__.py
+++ lib/pytest-lsp/pytest_lsp/__init__.py
@@ -1,6 +1,3 @@
-import asyncio
-import pytest
-
 from .client import Client
 from .client import make_test_client
 from .plugin import fixture
@@ -18,15 +15,3 @@ __all__ = [
     "make_client_server",
     "make_test_client",
 ]
-
-
-@pytest.fixture(scope="session")
-def event_loop():
-    # We need to redefine the event_loop fixture to match the scope of our
-    # client_server fixture.
-    #
-    # https://github.com/pytest-dev/pytest-asyncio/issues/68#issuecomment-334083751
-
-    loop = asyncio.get_event_loop_policy().new_event_loop()
-    yield loop
-    loop.close()
