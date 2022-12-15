42af6b3ee70d4d1b0e66d9ec3c009cbf100d1cdb
Alex Carney <alcarneyme@gmail.com>
2022-12-15 19:50:49 +0000

    pytest-lsp: Migrate to pygls v1.0
    Buried in here is also the migration to use the auto generated client as
    well as some fixes for issues where the tests hang due to a misbehaving
    server.

--- lib/pytest-lsp/pytest_lsp/check.py
+++ lib/pytest-lsp/pytest_lsp/check.py
@@ -2,27 +2,36 @@
 import logging
 from typing import List
 
-from pygls.lsp.types import *
+from lsprotocol.types import CompletionItem
+from lsprotocol.types import DocumentLink
+from lsprotocol.types import InsertTextFormat
+from lsprotocol.types import MarkupContent
+from pygls.capabilities import get_capability
 
-from pytest_lsp.client import Client
+from pytest_lsp.client import LanguageClient
 
 logger = logging.getLogger(__name__)
 
 
-def completion_items(client: Client, items: List[CompletionItem]):
+def completion_items(client: LanguageClient, items: List[CompletionItem]):
     """Ensure that the completion items returned from the server are compliant with the
     spec and the client's declared capabilities."""
 
-    commit_characters_support = client.capabilities.get_capability(
-        "text_document.completion.completion_item.commit_characters_support", False
+    commit_characters_support = get_capability(
+        client.capabilities,
+        "text_document.completion.completion_item.commit_characters_support",
+        False
     )
     documentation_formats = set(
-        client.capabilities.get_capability(
+        get_capability(
+            client.capabilities,
             "text_document.completion.completion_item.documentation_format", []
         )
     )
-    snippet_support = client.capabilities.get_capability(
-        "text_document.completion.completion_item.snippet_support", False
+    snippet_support = get_capability(
+        client.capabilities,
+        "text_document.completion.completion_item.snippet_support",
+        False
     )
 
     for item in items:
@@ -42,12 +51,14 @@ def completion_items(client: Client, items: List[CompletionItem]):
             assert snippet_support, "Client does not support snippets."
 
 
-def document_links(client: Client, items: List[DocumentLink]):
+def document_links(client: LanguageClient, items: List[DocumentLink]):
     """Ensure that the document links returned from the server are compliant with the
     spec and the client's declared capabilities."""
 
-    tooltip_support = client.capabilities.get_capability(
-        "text_document.document_link.tooltip_support", False
+    tooltip_support = get_capability(
+        client.capabilities,
+        "text_document.document_link.tooltip_support",
+        False
     )
 
     for item in items:
