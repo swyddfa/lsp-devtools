90cb5cf761cdc0c76e422152595cb526609da5e3
Alex Carney <alcarneyme@gmail.com>
2022-12-30 20:07:05 +0000

    lsp-devtools: Rewrite the `record` command to use `LSPAgentClient`
    This uses a modifed `RichHandler` from `rich` to output a live log to
    the console, drawing data via the new `LSPAgentClient`.
    
    I'm not convinced how useful the live console output is since a single
    completion response from `esbonio` is enough to swamp the scrollback
    buffer in the terminal. However, it at least proves the concept.
    
    Eventually this command can be extended with options to record a
    session to a file or SQLite database which I expect will be a lot more
    useful!

--- lib/lsp-devtools/lsp_devtools/cmds/record.py
+++ lib/lsp-devtools/lsp_devtools/cmds/record.py
@@ -1,83 +1,140 @@
-"""Record an LSP session."""
 import argparse
+import json
 import logging
-import subprocess
-import sys
 from typing import List
+from typing import Optional
 
-from lsp_devtools.agent import LSPAgent
-from lsp_devtools.agent import logger
+from rich.console import ConsoleRenderable
+from rich.containers import Renderables
+from rich.logging import RichHandler
+from rich.panel import Panel
+from rich.traceback import Traceback
+
+from lsp_devtools.agent import LSPAgentClient
+
+logger = logging.getLogger(__name__)
 
 
-class DebugOnly(logging.Filter):
-    """Only permits messages at debug level."""
+def obj_to_dict(obj):
+    if hasattr(obj, '_asdict'):
+        return {k: obj_to_dict(v) for k, v in obj._asdict().items()}
 
-    def filter(self, record: logging.LogRecord):
-        return record.levelno == logging.DEBUG and "source" in record.__dict__
+    if isinstance(obj, list):
+        return [obj_to_dict(o) for o in obj]
+
+    return obj
+
+
+class RichLSPHandler(RichHandler):
+
+    def __init__(self, level: int, log_time_format="%X", **kwargs):
+        super().__init__(
+            level=level,
+            show_path=False,
+            log_time_format=log_time_format,
+            omit_repeated_times=False,
+            **kwargs,
+        )
+
+    def render(
+        self, *,
+        record: logging.LogRecord,
+        traceback: Optional[Traceback],
+        message_renderable: "ConsoleRenderable"
+    ) -> "ConsoleRenderable":
+        """Render the complete log message, timestamps and all."""
+        msg = record.msg
+
+        # Override the log record's creation time with that of the lsp message.
+        record.created = msg.timestamp
+
+        # Delegate most of the rendering to the base RichHandler class.
+        res = super().render(
+            record=record, traceback=traceback, message_renderable=message_renderable
+        )
+
+        # Abuse the log level column to display the source of the message,
+        color = "green" if msg.source == "client" else "blue"
+        res.columns[1]._cells[0] = f"[bold][{color}]{msg.source.upper()}[/{color}][/bold]"
+
+        return res
+
+    def render_message(
+        self, record: logging.LogRecord, message: str
+    ) -> "ConsoleRenderable":
+        msg = record.msg
+        data = {}
+        data_title = ""
+
+        if msg.params:
+            data = msg.params
+            data_title = "params"
+
+        if msg.result:
+            data = msg.result
+            data_title = "result"
+
+        if msg.error:
+            data = msg.error
+            data_title = "error"
+
+        fields = [
+            f"id='{msg.id}'" if msg.id else "",
+            f"method='{msg.method}'" if msg.method else "",
+        ]
+
+        msg_data = json.dumps(obj_to_dict(data), indent=2)
+        return Renderables([
+            self.highlighter(" ".join([f for f in fields if f])),
+            Panel(self.highlighter(msg_data), title=data_title)
+        ])
+
+
+def log_message(params):
+    logger.info(params, extra={'some': 'test'})
+
+
+def setup_stdout_output():
+    handler = RichLSPHandler(logging.INFO)
+
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
 
 
 def record(args, extra: List[str]):
 
-    if extra is None:
-        print("Missing server start command", file=sys.stderr)
-        return 1
+    client = LSPAgentClient()
+    setup_stdout_output()
 
-    log_level = logging.DEBUG if args.raw else logging.INFO
-
-    process = subprocess.Popen(extra, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    logger.setLevel(log_level)
-
-    handler = logging.FileHandler(args.file, mode="w")
-    handler.setLevel(log_level)
-    handler.setFormatter(logging.Formatter(args.format))
-
-    if args.raw:
-        handler.addFilter(DebugOnly())
-
-    logger.addHandler(handler)
-
-    agent = LSPAgent(process, sys.stdin.buffer, sys.stdout.buffer)
-    agent.start()
-    agent.join()
-
+    client.lsp.fm.feature("$/lspMessage")(log_message)
+    client.start_ws_client(args.host, args.port)
 
 def cli(commands: argparse._SubParsersAction):
     cmd = commands.add_parser(
         "record",
-        help="record an LSP session.",
+        help="record an LSP session, requires the server be wrapped by an agent.",
         description="""\
-This command runs the given language server command as a subprocess and records all
-messages sent between client and server.
+This command connects to an LSP Agent allowing for messages sent
+between client and server to be logged.
 """,
     )  # type: argparse.ArgumentParser
 
-    output = cmd.add_mutually_exclusive_group()
-    output.add_argument(
-        "-f",
-        "--file",
-        help="save the log to a text file with the given filename",
-        default="lsp.log",
+    connect = cmd.add_argument_group(
+        title="connection options",
+        description="options that control the connection to the LSP Agent."
     )
-    # output.add_argument(
-    #     "--db",
-    #     help="save the log to a SQLite db with the given filename",
-    #     default=None,
-    # )
-
-    fileopts = cmd.add_argument_group(
-        title="file options",
-        description="these options only apply when --file is used",
+    connect.add_argument(
+        "--host",
+        type=str,
+        default="localhost",
+        help="the host that is hosting the agent."
     )
-    fileopts.add_argument(
-        "-r",
-        "--raw",
-        help="record all data, not just parsed messages",
-        action="store_true",
-    )
-    fileopts.add_argument(
-        "--format",
-        help="format string to use with the log messages",
-        default="%(message)s",
+    connect.add_argument(
+        "-p",
+        "--port",
+        type=int,
+        default=8765,
+        help="the port to connect to."
     )
 
     cmd.set_defaults(run=record)
