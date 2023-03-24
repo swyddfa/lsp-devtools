c77d8135ecdfe562e6feecbe9b3ed9de4f060c45
Alex Carney <alcarneyme@gmail.com>
2023-03-24 18:10:45 +0000

    pytest-lsp: Refactor how `ClientServer` works
    The `ClientServer` object no longer automatically initializes or
    shutdown the LSP session. It *does* however, still start and stop the
    client/server pair.
    
    In fact, the `ClientServer` object shouldn't rely on anything LSP
    specific, which should open the door to using `pytest_lsp` to test any
    JSON-RPC based protocol in the future.
    
    This means that fixture functions annotated with the
    `@pytest_lsp.fixture` decorator are now responsible for initializing
    and shutting down the LSP session, also granting the test author
    greater control over the initial setup.

--- lib/pytest-lsp/pytest_lsp/plugin.py
+++ lib/pytest-lsp/pytest_lsp/plugin.py
@@ -1,38 +1,23 @@
 import asyncio
 import inspect
-import json
 import logging
-import os
 import subprocess
 import sys
 import textwrap
 import threading
+import typing
 from concurrent.futures import ThreadPoolExecutor
-from typing import Any
 from typing import Callable
-from typing import Iterable
 from typing import List
 from typing import Optional
-from typing import Union
 
 import pytest
 import pytest_asyncio
-from lsprotocol.converters import get_converter
-from lsprotocol.types import ClientCapabilities
-from lsprotocol.types import InitializedParams
-from lsprotocol.types import InitializeParams
-from lsprotocol.types import LSPAny
 from pygls.server import StdOutTransportAdapter
 from pygls.server import aio_readline
 
 from pytest_lsp.client import LanguageClient
 from pytest_lsp.client import make_test_client
-
-if sys.version_info.minor < 9:
-    import importlib_resources as resources
-else:
-    import importlib.resources as resources  # type: ignore[no-redef]
-
 
 logger = logging.getLogger("client")
 
@@ -58,33 +43,17 @@ async def check_server_process(
 class ClientServer:
     """A client server pair used to drive test cases."""
 
-    def __init__(
-        self,
-        client: LanguageClient,
-        server: subprocess.Popen,
-        root_uri: str,
-        client_capabilities: ClientCapabilities,
-        initialization_options: Optional[LSPAny],
-    ):
+    def __init__(self, *, client: LanguageClient, server: subprocess.Popen):
         self.server = server
         """The process object running the server."""
 
         self.client = client
         """The client used to drive the test."""
 
-        self.client_capabilities = client_capabilities
-        """The capabilities of the client."""
-
-        self.initialization_options = initialization_options
-        """The initialization options to pass to the server."""
-
-        self.root_uri = root_uri
-        """The root uri to point the server at."""
-
         self._thread_pool_executor = ThreadPoolExecutor(max_workers=2)
         self._stop_event = threading.Event()
 
-    async def start(self):
+    def start(self):
         loop = asyncio.get_running_loop()
 
         self.client._stop_event = self._stop_event
@@ -117,29 +86,8 @@ class ClientServer:
             **watch_name,  # type: ignore[arg-type]
         )
 
-        response = await self.client.initialize_request(
-            InitializeParams(
-                process_id=os.getpid(),
-                root_uri=self.root_uri,
-                capabilities=self.client_capabilities,
-                initialization_options=self.initialization_options,
-            ),
-        )
-
-        assert response.capabilities is not None
-        self.client.notify_initialized(InitializedParams())
-
-        return response
-
     async def stop(self):
-        # Only attempt if there wasn't an error.
-        if self.client.error is None:
-            response = await self.client.shutdown_request(None)  # type: ignore
-            assert response is None
-
-            self.client.notify_exit(None)
-        else:
-            self.server.terminate()
+        self.server.terminate()
 
         if self.client._stop_event:
             self.client._stop_event.set()
@@ -154,12 +102,8 @@ class ClientServerConfig:
     def __init__(
         self,
         server_command: List[str],
-        root_uri: str,
         *,
-        client: str = "",
-        client_capabilities: Optional[ClientCapabilities] = None,
-        client_factory: Callable[..., LanguageClient] = make_test_client,
-        initialization_options: Optional[Any] = None,
+        client_factory: Callable[[], LanguageClient] = make_test_client,
     ) -> None:
         """
         Parameters
@@ -167,57 +111,13 @@ class ClientServerConfig:
         server_command
            The command to use to start the language server.
 
-        root_uri
-           The root uri to start the language server in
-
-        client
-           The name of the client profile to use
-
-        client_capabilities
-           Use to use a specific set of client, capabilities.
-           Specifiying this will override ``client``.
-
         client_factory
            Factory function to use when constructing the language client instance.
            Defaults to :func:`pytest_lsp.make_test_client`
-
-        initialization_options
-           The initialization options to pass to the server on start up.
-
         """
 
         self.server_command = server_command
-        self.root_uri = root_uri
-        self.client = client
-        self.client_capabilities = client_capabilities
         self.client_factory = client_factory
-        self.initialization_options = initialization_options
-
-
-def find_client_capabilities(client: str) -> ClientCapabilities:
-    """Find the capabilities that correspond to the given client spec."""
-
-    # Currently, we only have a single version of each client so let's just return the
-    # first one we find.
-    #
-    # TODO: Implement support for client@x.y.z
-    # TODO: Implement support for client@latest?
-    filename = None
-    for resource in resources.files("pytest_lsp.clients").iterdir():
-        # Skip the README or any other files that we don't care about.
-        if not resource.name.endswith(".json"):
-            continue
-
-        if resource.name.startswith(client.replace("-", "_")):
-            filename = resource
-            break
-
-    if not filename:
-        raise ValueError(f"Unsupported client '{client}'")
-
-    converter = get_converter()
-    capabilities = json.loads(filename.read_text())
-    return converter.structure(capabilities, ClientCapabilities)
 
 
 def make_client_server(config: ClientServerConfig) -> ClientServer:
@@ -230,21 +130,9 @@ def make_client_server(config: ClientServerConfig) -> ClientServer:
         stderr=subprocess.PIPE,
     )
 
-    if config.client_capabilities:
-        capabilities = config.client_capabilities
-    elif config.client:
-        capabilities = find_client_capabilities(config.client)
-    else:
-        capabilities = ClientCapabilities()
-
-    client = config.client_factory(capabilities, config.root_uri)
-
     return ClientServer(
-        client=client,
         server=server,
-        client_capabilities=capabilities,
-        root_uri=config.root_uri,
-        initialization_options=config.initialization_options,
+        client=config.client_factory(),
     )
 
 
@@ -299,34 +187,36 @@ def pytest_runtest_makereport(item: pytest.Item, call: pytest.CallInfo):
 def fixture(
     fixture_function=None,
     *,
-    config: Union[ClientServerConfig, Iterable[ClientServerConfig]],
+    config: ClientServerConfig,
     **kwargs,
 ):
-    if isinstance(config, ClientServerConfig):
-        params = [config]
-    else:
-        params = list(config)
-
-    ids = [conf.client or f"client{idx}" for idx, conf in enumerate(params)]
-
     def wrapper(fn):
-        @pytest_asyncio.fixture(params=params, ids=ids, **kwargs)
+        @pytest_asyncio.fixture(**kwargs)
         async def the_fixture(request):
-            lsp = make_client_server(request.param)
-            await lsp.start()
+            client_server = make_client_server(config)
+            client_server.start()
 
-            # TODO: Do this 'properly'
-            signature = inspect.signature(fn)
-            if "client_" in signature.parameters.keys():
-                await fn(lsp.client)
-            else:
-                await fn()
+            kwargs = {}
+            for name, cls in typing.get_type_hints(fn).items():
+                if issubclass(cls, LanguageClient):
+                    kwargs[name] = client_server.client
 
-            lsp.client._setup_log_index = len(lsp.client.log_messages)
-            lsp.client._last_log_index = len(lsp.client.log_messages)
+            result = fn(**kwargs)
+            if inspect.isasyncgen(result):
+                try:
+                    await anext(result)
+                except StopAsyncIteration:
+                    pass
 
-            yield lsp.client
-            await lsp.stop()
+            yield client_server.client
+
+            if inspect.isasyncgen(result):
+                try:
+                    await anext(result)
+                except StopAsyncIteration:
+                    pass
+
+            await client_server.stop()
 
         return the_fixture
 
