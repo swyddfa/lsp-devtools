791ed2ec345302f8622a551e46068bf150ed669a
Alex Carney <alcarneyme@gmail.com>
2023-06-22 20:32:03 +0100

    lsp-devtools: Align tui app to new architecture
    The tui app ditches the logging.Handler setup entirely.
    
    Instead the app now hosts an async TCP, JSON-RPC server for LSP agents
    to connect to. Upon receiving messages the new centralised `Database`
    object is responsible for saving the messages to the underlying
    SQLite DB.

--- lib/lsp-devtools/lsp_devtools/tui/__init__.py
+++ lib/lsp-devtools/lsp_devtools/tui/__init__.py
@@ -1,23 +1,24 @@
 import argparse
 import asyncio
 import json
+import logging
 import pathlib
-import threading
+import re
 from datetime import datetime
 from typing import Any
 from typing import Dict
 from typing import List
-from typing import Optional
-from typing import Tuple
 
-import aiosqlite
 import appdirs
 from rich.highlighter import ReprHighlighter
 from rich.text import Text
 from textual import events
+from textual import log
+from textual import on
 from textual.app import App
 from textual.app import ComposeResult
 from textual.containers import Container
+from textual.containers import ScrollableContainer
 from textual.events import Ready
 from textual.widgets import DataTable
 from textual.widgets import Footer
@@ -25,14 +26,18 @@ from textual.widgets import Header
 from textual.widgets import Tree
 from textual.widgets.tree import TreeNode
 
-from lsp_devtools.record import setup_filter_args
+from lsp_devtools.agent import MESSAGE_TEXT_NOTIFICATION
+from lsp_devtools.agent import AgentServer
+from lsp_devtools.agent import MessageText
+from lsp_devtools.handlers import LspMessage
 
-from .client import Ping
-from .client import TUIAgentClient
-from .client import connect_to_agent
+from .database import Database
+from .database import PingMessage
+
+logger = logging.getLogger(__name__)
 
 
-class ObjectViewer(Tree):
+class MessageViewer(Tree):
     """Used to inspect the fields of an object."""
 
     def __init__(self, *args, **kwargs) -> None:
@@ -63,19 +68,15 @@ class ObjectViewer(Tree):
             node.set_label(Text.assemble(label, " = ", self.highlighter(repr(obj))))
 
 
-RPCData = Dict[int, Tuple[Optional[str], Optional[str], Optional[str]]]
-
-
 class MessagesTable(DataTable):
     """Datatable used to display all messages between client and server"""
 
-    def __init__(self, dbpath: pathlib.Path, viewer: ObjectViewer):
+    def __init__(self, db: Database, viewer: MessageViewer):
         super().__init__()
 
-        self.dbpath = dbpath
-        self.dbquery = "SELECT rowid, * FROM protocol WHERE rowid > ?"
-        self.rpcdata: RPCData = {}
-        self.max_row = -1
+        self.db = db
+        self.rpcdata: Dict[int, LspMessage] = {}
+        self.max_row = 0
 
         self.viewer = viewer
 
@@ -92,50 +93,42 @@ class MessagesTable(DataTable):
             return
 
         rowid = int(self.get_row_at(self.cursor_row)[0])
-        params, result, error = self.rpcdata[rowid]
+        message = self.rpcdata[rowid]
+        name = ""
+        obj = {}
 
-        if params:
+        if message.params:
             name = "params"
-            message = json.loads(params)
+            obj = message.params
 
-        elif result:
+        elif message.result:
             name = "result"
-            message = json.loads(result)
+            obj = message.result
 
-        elif error:
+        elif message.error:
             name = "error"
-            message = json.loads(error)
+            obj = message.error
 
-        else:
-            name = "data"
-            message = {}
-
-        self.viewer.set_object(name, message)
+        self.viewer.set_object(name, obj)
 
     async def update(self):
         """Trigger a re-run of the query to pull in new data."""
 
-        async with aiosqlite.connect(self.dbpath) as conn:
-            async with conn.execute(self.dbquery, (self.max_row,)) as cursor:
-                async for row in cursor:
-                    rowid = row[0]
-                    timestamp = row[2]
-                    source = row[3]
-                    id_ = row[4]
-                    method = row[5]
-                    params = row[6]
-                    result = row[7]
-                    error = row[8]
+        messages = await self.db.get_messages(self.max_row - 1)
+        for message in messages:
+            self.max_row += 1
+            self.rpcdata[self.max_row] = message
 
-                    self.rpcdata[rowid] = (params, result, error)
+            # Surely there's a more direct way to do this?
+            dt = datetime.fromtimestamp(message.timestamp)
+            time = dt.isoformat(timespec="milliseconds")
+            time = time[time.find("T") + 1 :]
 
-                    # Surely there's a more direct way to do this?
-                    dt = datetime.fromtimestamp(timestamp)
-                    time = dt.isoformat(timespec="milliseconds")
-                    time = time[time.find("T") + 1 :]
+            self.add_row(
+                str(self.max_row), time, message.source, message.id, message.method
+            )
 
-                    self.add_row(str(rowid), time, source, id_, method)
-                    self.max_row = rowid
+        self.move_cursor(row=self.max_row, animate=True)
 
 
 class Sidebar(Container):
@@ -144,28 +137,26 @@ class Sidebar(Container):
 
 class LSPInspector(App):
     CSS_PATH = pathlib.Path(__file__).parent / "app.css"
-    BINDINGS = [("ctrl+b", "toggle_sidebar", "Sidebar"), ("q", "quit", "Quit")]
+    BINDINGS = [("ctrl+b", "toggle_sidebar", "Sidebar"), ("ctrl+c", "quit", "Quit")]
 
-    def __init__(self, dbpath: pathlib.Path, *args, **kwargs):
+    def __init__(self, db: Database, server: AgentServer, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.dbpath = dbpath
+        self.db = db
+        self.db.app = self
         """Where the data for the app is being held"""
 
-        self.client: Optional[TUIAgentClient] = None
-        """Client used to interact with the LSPAgent hosting the server we're
-        inspecting."""
+        self.server = server
+        """Server used to manage connections to lsp servers."""
 
-        self.loop: Optional[asyncio.AbstractEventLoop] = None
-        """Accessed by the AgentClient to push messages into the UI"""
+        self._async_tasks = []
 
     def compose(self) -> ComposeResult:
         yield Header()
 
-        viewer = ObjectViewer("")
-        messages = MessagesTable(self.dbpath, viewer)
-
-        yield Container(messages, Sidebar(viewer))
+        viewer = MessageViewer("")
+        messages = MessagesTable(self.db, viewer)
+        yield Container(ScrollableContainer(messages), Sidebar(viewer))
         yield Footer()
 
     def action_toggle_sidebar(self) -> None:
@@ -179,12 +170,14 @@ class LSPInspector(App):
                 self.screen.set_focus(None)
             sidebar.add_class("-hidden")
 
-    async def on_ready(self, event: Ready):
-        self.loop = asyncio.get_running_loop()
+    @on(PingMessage)
+    async def on_ping(self, message: PingMessage):
         await self.update_table()
 
-    async def on_ping(self, message: Ping):
-        """Fired when the agent client receives new messages"""
+    async def on_ready(self, event: Ready):
+        self._async_tasks.append(
+            asyncio.create_task(self.server.start_tcp("localhost", 8765))
+        )
         await self.update_table()
 
     async def update_table(self):
@@ -192,35 +185,64 @@ class LSPInspector(App):
         await table.update()
 
     async def action_quit(self):
-        if self.client:
-            self.client._stop_event.set()
+        await self.server.stop()
+        await self.db.close()
         await super().action_quit()
 
 
-def start_client(client, host, port):
-    try:
-        client.start_ws_client(host, port)
-    except Exception:
-        # TODO: Surface the error somehow
-        pass
+MESSAGE_PATTERN = re.compile(
+    r"^(?:[^\r\n]+\r\n)*"
+    + r"Content-Length: (?P<length>\d+)\r\n"
+    + r"(?:[^\r\n]+\r\n)*\r\n"
+    + r"(?P<body>{.*)",
+    re.DOTALL,
+)
+
+
+async def handle_message(ls: AgentServer, message: MessageText):
+    """Handle messages received from the connected lsp server."""
+
+    data = message.text
+    message_buf = ls._client_buffer if message.source == "client" else ls._server_buffer
+
+    while len(data):
+        # Append the incoming chunk to the message buffer
+        message_buf.append(data)
+
+        # Look for the body of the message
+        msg = "".join(message_buf)
+        found = MESSAGE_PATTERN.fullmatch(msg)
+
+        body = found.group("body") if found else ""
+        length = int(found.group("length")) if found else 1
+
+        if len(body) < length:
+            # Message is incomplete; bail until more data arrives
+            return
+
+        # Message is complete;
+        # extract the body and any remaining data,
+        # and reset the buffer for the next message
+        body, data = body[:length], body[length:]
+        message_buf.clear()
+
+        rpc = json.loads(body)
+        await ls.db.add_message(message.session, message.timestamp, message.source, rpc)
+
+
+def setup_server(db: Database):
+    server = AgentServer()
+    server.db = db
+    server.feature(MESSAGE_TEXT_NOTIFICATION)(handle_message)
+    return server
 
 
 def tui(args, extra: List[str]):
-    dbpath = args.to_sqlite
-    if not dbpath.parent.exists():
-        dbpath.parent.mkdir(parents=True)
+    db = Database(args.dbpath)
+    server = setup_server(db)
 
-    app = LSPInspector(dbpath)
-    client = connect_to_agent(args, app)
-    app.client = client
-
-    agent_thread = threading.Thread(
-        name="AgentClient", target=start_client, args=(client, args.host, args.port)
-    )
-    agent_thread.start()
-
+    app = LSPInspector(db, server)
     app.run()
-    agent_thread.join()
 
 
 def cli(commands: argparse._SubParsersAction):
@@ -242,7 +264,6 @@ manipulate an LSP session interactively.
         type=pathlib.Path,
         metavar="DB",
         default=default_db,
-        dest="to_sqlite",  # to be compatible with code borrowed from record command.
         help="the database path to use",
     )
 
@@ -259,6 +280,4 @@ manipulate an LSP session interactively.
     connect.add_argument(
         "-p", "--port", type=int, default=8765, help="the port to connect to."
     )
-
-    setup_filter_args(cmd)
     cmd.set_defaults(run=tui)
