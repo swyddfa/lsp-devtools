963d1cab16b0de41407645c33cfc5907b25b5ab8
Alex Carney <alcarneyme@gmail.com>
2023-07-02 00:05:37 +0100

    docs: Add "this is the unstable docs" banner

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,9 @@ sys.path.insert(0, os.path.abspath("./ext"))
 from docutils import nodes  # noqa: E402
 from sphinx.application import Sphinx  # noqa: E402
 
+DEV_BUILD = os.getenv("BUILDDIR", None) == "latest"
+BRANCH = "develop" if DEV_BUILD else "release"
+
 project = "LSP Devtools"
 copyright = "2023, Alex Carney"
 author = "Alex Carney"
@@ -48,6 +51,19 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 html_theme = "furo"
 html_title = "LSP Devtools"
 # html_static_path = ["_static"]
+html_theme_options = {
+    "source_repository": "https://github.com/swyddfa/lsp-devtools/",
+    "source_branch": BRANCH,
+    "source_directory": "docs/",
+}
+
+if DEV_BUILD:
+    html_theme_options["announcement"] = (
+        "This is the unstable version of the documentation, features may change or "
+        "be removed without warning. "
+        '<a href="/lsp-devtools/docs/stable/en/">Click here</a> '
+        "to view the released version"
+    )
 
 
 def lsp_role(name, rawtext, text, lineno, inliner, options={}, content=[]):
