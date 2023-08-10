4b281c0cd05f158e8d3faf750f171e07547b0e42
Alex Carney <alcarneyme@gmail.com>
2023-08-10 20:41:22 +0100

    pytest-lsp: Passthrough requested fixtures to user fixture func

---
+++ lib/pytest-lsp/tests/examples/fixture-passthrough/t_server.py
@@ -0,0 +1,52 @@
+import sys
+
+import pytest
+from lsprotocol.types import CompletionList
+from lsprotocol.types import CompletionParams
+from lsprotocol.types import InitializeParams
+from lsprotocol.types import Position
+from lsprotocol.types import TextDocumentIdentifier
+
+import pytest_lsp
+from pytest_lsp import ClientServerConfig
+from pytest_lsp import LanguageClient
+from pytest_lsp import client_capabilities
+
+
+@pytest.fixture(scope="module")
+def client_name():
+    return "neovim"
+
+
+@pytest_lsp.fixture(
+    config=ClientServerConfig(server_command=[sys.executable, "server.py"]),
+)
+async def client(client_name: str, lsp_client: LanguageClient):
+    # Setup
+    params = InitializeParams(capabilities=client_capabilities(client_name))
+    await lsp_client.initialize_session(params)
+
+    yield
+
+    # Teardown
+    await lsp_client.shutdown_session()
+
+
+async def test_completions(client: LanguageClient):
+    """Ensure that the server implements completions correctly."""
+
+    results = await client.text_document_completion_async(
+        params=CompletionParams(
+            position=Position(line=1, character=0),
+            text_document=TextDocumentIdentifier(uri="file:///path/to/file.txt"),
+        )
+    )
+    assert results is not None
+
+    if isinstance(results, CompletionList):
+        items = results.items
+    else:
+        items = results
+
+    labels = [item.label for item in items]
+    assert labels == ["hello", "world"]
