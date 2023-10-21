5a800cbdd8d276ec6bd01d9bc31c7e1ea47f80b2
Alex Carney <alcarneyme@gmail.com>
2023-10-21 15:12:21 +0100

    pytest-lsp: Add syntax for selecting a specific version of a client

--- lib/pytest-lsp/tests/test_client.py
+++ lib/pytest-lsp/tests/test_client.py
@@ -5,23 +5,43 @@ import sys
 
 import pygls.uris as uri
 import pytest
+
 import pytest_lsp
 
 
 @pytest.mark.parametrize(
-    "client_spec,client_capabilities",
+    "client_spec,capabilities",
     [
         *itertools.product(
             ["visual_studio_code", "visual-studio-code"],
             ["visual_studio_code_v1.65.2.json"],
         ),
-        ("neovim", "neovim_v0.6.1.json"),
+        *itertools.product(
+            ["neovim@0.6", "neovim@v0.6", "neovim@0.6.1"],
+            ["neovim_v0.6.1.json"],
+        ),
+        *itertools.product(
+            ["neovim", "neovim@latest", "neovim@v0", "neovim@v0.9", "neovim@v0.9.1"],
+            ["neovim_v0.9.1.json"],
+        ),
     ],
 )
 def test_client_capabilities(
-    pytester: pytest.Pytester, client_spec: str, client_capabilities: str
+    pytester: pytest.Pytester, client_spec: str, capabilities: str
 ):
-    """Ensure that the plugin can mimic the requested client's capabilities."""
+    """Ensure that the plugin can mimic the requested client's capabilities correctly.
+
+    Parameters
+    ----------
+    pytester
+       pytest's built in pytester fixture.
+
+    client_spec
+       The string used to select the desired client and version
+
+    client_capabilities
+       The filename containing the expected client capabilities
+    """
 
     python = sys.executable
     testdir = pathlib.Path(__file__).parent
@@ -29,7 +49,7 @@ def test_client_capabilities(
     root_uri = uri.from_fs_path(str(testdir))
 
     clients_dir = pathlib.Path(pytest_lsp.__file__).parent / "clients"
-    with (clients_dir / client_capabilities).open() as f:
+    with (clients_dir / capabilities).open() as f:
         # Easiest way to reformat the JSON onto a single line
         expected = json.dumps(json.load(f)["capabilities"])
 
@@ -71,14 +91,23 @@ async def client(lsp_client: LanguageClient):
         f"""
 import json
 import pytest
-from lsprotocol.types import ExecuteCommandParams
+from lsprotocol import types
+from lsprotocol.converters import get_converter
 
 @pytest.mark.asyncio
 async def test_capabilities(client):
     actual = await client.workspace_execute_command_async(
-        ExecuteCommandParams(command="return.client.capabilities")
+        types.ExecuteCommandParams(command="return.client.capabilities")
     )
-    assert actual == json.loads('{expected}')
+
+    expected = json.loads('{expected}')
+
+    # lsprotocol is going to filter out any quirks of the client
+    # so we can't compare the dicts directly.
+    converter = get_converter()
+    actual_capabilities = converter.structure(actual, types.ClientCapabilities)
+    expected_capabilities = converter.structure(expected, types.ClientCapabilities)
+    assert actual_capabilities == expected_capabilities
     """
     )
 
