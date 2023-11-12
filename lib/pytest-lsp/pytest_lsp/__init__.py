f3fa13ed289de8bca946e6900682b574770cb3c1
Alex Carney <alcarneyme@gmail.com>
2023-11-12 14:47:08 +0000

    pytest-lsp: Add option to integrate with `lsp-devtools`

--- lib/pytest-lsp/pytest_lsp/__init__.py
+++ lib/pytest-lsp/pytest_lsp/__init__.py
@@ -5,6 +5,7 @@ from .client import client_capabilities
 from .client import make_test_lsp_client
 from .plugin import ClientServerConfig
 from .plugin import fixture
+from .plugin import pytest_addoption
 from .plugin import pytest_runtest_makereport
 from .protocol import LanguageClientProtocol
 
@@ -17,5 +18,6 @@ __all__ = [
     "client_capabilities",
     "fixture",
     "make_test_lsp_client",
+    "pytest_addoption",
     "pytest_runtest_makereport",
 ]
