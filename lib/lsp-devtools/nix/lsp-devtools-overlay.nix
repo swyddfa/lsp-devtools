5e2a5b9038a8206df1e0b153ae05d928909d4a88
Alex Carney <alcarneyme@gmail.com>
2024-01-07 18:02:08 +0000

    nix: Package stamina in nix

--- lib/lsp-devtools/nix/lsp-devtools-overlay.nix
+++ lib/lsp-devtools/nix/lsp-devtools-overlay.nix
@@ -9,6 +9,38 @@ in {
   pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [(
     python-final: python-prev: {
 
+      stamina = python-prev.buildPythonPackage rec {
+        pname = "stamina";
+        version = "24.1.0";
+        format = "pyproject";
+
+        src = prev.fetchFromGitHub {
+          owner = "hynek";
+          repo = "stamina";
+          rev = "refs/tags/${version}";
+          hash = "sha256-bIVzE9/QsdGw/UE83q3Q/XG3jFnPy65pkDdMpYkIrrs=";
+        };
+
+        SETUPTOOLS_SCM_PRETEND_VERSION = version;
+        nativeBuildInputs = with python-final; [
+          hatchling
+          hatch-vcs
+          hatch-fancy-pypi-readme
+        ];
+
+        propagatedBuildInputs = with python-final; [
+          tenacity
+        ] ++ prev.lib.optional (pythonOlder "3.10") typing-extensions;
+
+        doCheck = true;
+        pythonImportsCheck = [ "stamina" ];
+        nativeCheckInputs = with python-prev; [
+          anyio
+          pytestCheckHook
+        ];
+
+      };
+
       lsp-devtools = python-prev.buildPythonPackage {
         pname = "lsp-devtools";
         version = versionStr;
