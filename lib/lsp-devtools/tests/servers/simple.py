9a566be5050322096e68ab4c9bc20ea77f74e939
Alex Carney <alcarneyme@gmail.com>
2024-01-27 17:47:12 +0000

    lsp-devtools: Ensure the agent stops once the server process exits

---
+++ lib/lsp-devtools/tests/servers/simple.py
@@ -0,0 +1,14 @@
+"""A very simple language server."""
+from lsprotocol import types
+from pygls.server import LanguageServer
+
+server = LanguageServer("simple-server", "v1")
+
+
+@server.feature(types.INITIALIZED)
+def _(ls: LanguageServer, params: types.InitializedParams):
+    ls.show_message("Hello, world!")
+
+
+if __name__ == "__main__":
+    server.start_io()
