85a137bb510654d546ba9a0ab5c811b3f8789d7d
Alex Carney <alcarneyme@gmail.com>
2025-10-14 20:56:17 +0100

    lsp-devtools: don't base filters on logging system

--- lib/lsp-devtools/lsp_devtools/record/filters.py
+++ lib/lsp-devtools/lsp_devtools/record/filters.py
@@ -5,28 +5,52 @@ import typing
 
 import attrs
 
-from .formatters import FormatString
+from lsp_devtools.agent import MessageSource
 
 if typing.TYPE_CHECKING:
     from typing import Literal
 
-    MessageSource = Literal["client", "server", "both"]
-    MessageType = Literal["request", "response", "result", "error", "notification"]
+    from lsp_devtools.agent import JsonRPCMessage
+    from lsp_devtools.agent.server import JsonRPCMessageType
+
+    MessageSourceString = Literal["client", "server", "both"]
+
 
 logger = logging.getLogger(__name__)
 
 
-@attrs.define
-class LSPFilter(logging.Filter):
-    """Logging filter for LSP messages."""
+def string_to_message_source(
+    value: MessageSourceString,
+) -> Literal["both"] | MessageSource:
+    if value == "both":
+        return "both"
 
-    message_source: MessageSource = attrs.field(default="both")
+    if value == "client":
+        return MessageSource.Client
+
+    if value == "server":
+        return MessageSource.Server
+
+    raise ValueError(f"Unknown message source: {value!r}")
+
+
+@attrs.define
+class JsonRPCFilter:
+    """Logging filter for JSON-RPC messages."""
+
+    message_source: Literal["both"] | MessageSource = attrs.field(
+        default="both", converter=string_to_message_source
+    )
     """Only include messages from the given source."""
 
-    include_message_types: set[MessageType] = attrs.field(factory=set, converter=set)
+    include_message_types: set[JsonRPCMessageType] = attrs.field(
+        factory=set, converter=set
+    )
     """Only include the given message types."""
 
-    exclude_message_types: set[MessageType] = attrs.field(factory=set, converter=set)
+    exclude_message_types: set[JsonRPCMessageType] = attrs.field(
+        factory=set, converter=set
+    )
     """Exclude the given message types."""
 
     include_methods: set[str] = attrs.field(factory=set, converter=set)
@@ -35,69 +59,57 @@ class LSPFilter(logging.Filter):
     exclude_methods: set[str] = attrs.field(factory=set, converter=set)
     """Exclude messages associated with the given method."""
 
-    formatter: FormatString = attrs.field(
-        default="",
-        converter=FormatString,
-    )  # type: ignore
-    """Format messages according to the given string"""
-
     _response_method_map: dict[int | str, str] = attrs.field(factory=dict)
     """Used to determine the method for response messages"""
 
-    def filter(self, record: logging.LogRecord) -> bool:
-        message = record.args
-        if not isinstance(message, dict):
-            return False
+    def match(self, message: JsonRPCMessage) -> JsonRPCMessage | None:
+        """Return the given message if it passes the configured filters"""
+        source = message.metadata["source"]
+        message_type = message.msg_type
+        message_method = self._get_message_method(message)
 
-        source = record.__dict__["Message-Source"]
-        message_type = get_message_type(message)
-        message_method = self._get_message_method(message_type, message)
-
-        if self.message_source not in {"both", source}:
-            return False
+        if not self.source_matches(source):
+            return None
 
         if self.include_message_types and not message_matches_type(
             message_type, self.include_message_types
         ):
-            return False
+            return None
 
         if self.exclude_message_types and message_matches_type(
             message_type, self.exclude_message_types
         ):
-            return False
+            return None
 
         if self.include_methods and message_method not in self.include_methods:
-            return False
+            return None
 
         if self.exclude_methods and message_method in self.exclude_methods:
+            return None
+
+        return message
+
+    def source_matches(self, source: MessageSource):
+        if source == MessageSource.Agent:
             return False
 
-        if self.formatter.pattern:
-            try:
-                record.msg = self.formatter.format(message)
-                record.args = None
-            except Exception:
-                logger.debug(
-                    "Skipping message that failed to format: %s", message, exc_info=True
-                )
-                return False
+        return self.message_source in {"both", source}
 
-        return True
+    def _get_message_method(self, message: JsonRPCMessage) -> str | None:
+        method = message.method
+        msg_id = message.msg_id
 
-    def _get_message_method(self, message_type: str, message: dict) -> str:
-        if message_type == "request":
-            method = message["method"]
-            self._response_method_map[message["id"]] = method
+        if message.msg_type in {"request", "notification"}:
+            if msg_id is not None and method is not None:
+                self._response_method_map[msg_id] = method
 
             return method
 
-        if message_type == "notification":
-            return message["method"]
-
-        return self._response_method_map[message["id"]]
+        if msg_id is not None:
+            return self._response_method_map.get(msg_id)
 
 
-def message_matches_type(message_type: str, types: set[MessageType]) -> bool:
+def message_matches_type(message_type: str, types: set[JsonRPCMessageType]) -> bool:
     """Determine if the type of message is included in the given set of types"""
 
     if message_type == "result":
@@ -107,15 +119,3 @@ def message_matches_type(message_type: str, types: set[MessageType]) -> bool:
         return len({"error", "response"} & types) > 0
 
     return message_type in types
-
-
-def get_message_type(message: dict) -> str:
-    if "id" in message:
-        if "error" in message:
-            return "error"
-        elif "method" in message:
-            return "request"
-        else:
-            return "result"
-    else:
-        return "notification"
