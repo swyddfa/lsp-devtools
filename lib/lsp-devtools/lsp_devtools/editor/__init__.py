2620fd0ffbe60812fa5f44c9d8e024feb03512f4
Alex Carney <alcarneyme@gmail.com>
2025-12-11 20:25:56 +0000

    lsp-devtools: basic implementation of opening text documents

--- lib/lsp-devtools/lsp_devtools/editor/__init__.py
+++ lib/lsp-devtools/lsp_devtools/editor/__init__.py
@@ -1,7 +1,11 @@
+from .explorer import Explorer
+from .panel import OutputWindow
 from .panel import Panel
 from .view import TextEditorView
 
 __all__ = (
+    "Explorer",
     "Panel",
+    "OutputWindow",
     "TextEditorView",
 )
