21bb532e9b5c8cef818c34f8318bf1e4224acfd4
Alex Carney <alcarneyme@gmail.com>
2025-12-11 20:45:26 +0000

    lsp-devtools: basic implementation of sending changes

--- lib/lsp-devtools/lsp_devtools/editor/view.py
+++ lib/lsp-devtools/lsp_devtools/editor/view.py
@@ -11,20 +11,33 @@ if typing.TYPE_CHECKING:
 
     from pygls.lsp.client import LanguageClient
     from textual.app import ComposeResult
+    from textual.widgets.text_area import Edit
+    from textual.widgets.text_area import EditResult
 
 
 class TextEditorView(Container):
     """A view containing the text editor"""
 
+    def compose(self) -> ComposeResult:
+        text_area = LspTextArea()
+        text_area.show_line_numbers = True
+        yield text_area
+
+    def open_text_document(self, path: pathlib.Path):
+        """Open the given text document."""
+
+        editor = self.query_one(LspTextArea)
+        editor.open_text_document(path)
+
+
+class LspTextArea(TextArea):
+    """A lsp-enabled text area"""
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.path: pathlib.Path | None = None
-
-    def compose(self) -> ComposeResult:
-        text_area = TextArea()
-        text_area.show_line_numbers = True
-        yield text_area
+        self.version = -1
 
     @property
     def client(self) -> LanguageClient | None:
@@ -44,8 +57,7 @@ class TextEditorView(Container):
                 )
             )
 
-        editor = self.query_one(TextArea)
-        editor.text = text
+        self.load_text(text)
         self.path = path
 
         if self.client is not None:
@@ -59,3 +71,37 @@ class TextEditorView(Container):
                     )
                 )
             )
+
+    def edit(self, edit: Edit) -> EditResult:
+        """Extend the base ``edit()`` method to.
+
+        - Ensure that any edits that are made to the document are syncronised with the
+          server.
+        """
+        result = super().edit(edit)
+
+        if self.path is None or self.client is None:
+            return result
+
+        self.version += 1
+        start_line, start_col = edit.from_location
+        end_line, end_col = edit.to_location
+
+        self.client.text_document_did_change(
+            types.DidChangeTextDocumentParams(
+                text_document=types.VersionedTextDocumentIdentifier(
+                    version=self.version, uri=self.path.as_uri()
+                ),
+                content_changes=[
+                    types.TextDocumentContentChangePartial(
+                        text=edit.text,
+                        range=types.Range(
+                            start=types.Position(line=start_line, character=start_col),
+                            end=types.Position(line=end_line, character=end_col),
+                        ),
+                    )
+                ],
+            )
+        )
+
+        return result
